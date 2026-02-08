2730bd19f19a659ba4ceed8b106986bf26a06508
Ayushi-011 <101706115+Ayushi-011@users.noreply.github.com>
2026-02-08 16:20:13 +0530

    ip_service.py

--- services/ip_service.py
+++ services/ip_service.py
@@ -1,7 +1,9 @@
 from fastapi import Request
 import httpx
 
-async def get_ip(ip1:Request):
+async def get_ip(ip1:Request, ip2:str=None):
+    if ip2:
+        return ip2
     sent = ip1.headers.get("X-Forwarded-For")
     if sent : 
         return sent.split(",")[0].strip()
@@ -11,4 +13,5 @@ async def diff_ip():
     async with httpx.AsyncClient() as client1:
         res = await client1.get("https://api.ipify.org?format=text")
         return res.text
+
 
