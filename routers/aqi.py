7766904cc2af3b3c2753169e6cb53408ca5f96c5
Ayushi-011 <101706115+Ayushi-011@users.noreply.github.com>
2026-02-08 15:16:36 +0530

    Routers

---
+++ routers/aqi.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter,Request
+from services.ip_service import diff_ip
+from services.geo_service import get_loc
+from services.aqi_service import get_aqi
+from models.schemas import Op
+
+router = APIRouter()
+
+@router.get("/aqi")
+async def home( ip1:Request, Response_model=Op):
+    ip = await diff_ip()
+    loc = await get_loc(ip)
+    aqi = await get_aqi(loc.get("latitude"),loc.get("longitude"))
+    category = "Not-Defined"
+    if int(aqi)<50:
+        category = "Good AQI"
+    elif int(aqi)<100:
+        category = "Moderate AQI"
+    elif int(aqi)<150:
+        category = "Unhealthy for Sensitive Groups"
+    elif int(aqi)<200:
+        category = "Unhealthy AQI"
+    elif int(aqi)<300:
+        category = "Very Unhealthy AQI"
+    else:
+        category = "Hazardous AQI" 
+
+
+    return {
+        "ip" : ip,
+        "location" :
+        {
+            "city": loc.get("city"),
+            "region": loc.get("region"),
+            "country": loc.get("country"),
+            "lat": loc.get("latitude"),
+            "lon": loc.get("longitude"),
+        },
+        "aqi" :
+        {
+            "value" : aqi,
+            "category" : category,
+        },
+        "src" : "open-meteo"
+    } 
+
+    
